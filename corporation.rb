a22e40ba47f5afece5b03216a6f7affcddafc125
tokyosuite <helenyueyu@gmail.com>
2020-03-08 11:17:14 -0700

    add board

--- corporation.rb
+++ corporation.rb
@@ -1,11 +1,37 @@
 class Corporation 
-    def initialize
+    attr_reader :terraform_rating, :money_production, :steel_production, :titanium_production, :plant_production, :energy_production, :heat_production 
+
+    attr_reader :money, :steel, :titanium, :plant, :energy, :heat 
+
+    def initialize(player)
+        @player = player 
+
         @terraform_rating = 20 
+        @money_production = 0 
         @steel_production = 0 
         @titanium_production = 0 
         @plant_production = 0 
-        @energy_production = 1 
-        @heat_production = 1 
+        @energy_production = 0
+        @heat_production = 0
+
+        @money = 0 
+        @steel = 0 
+        @titanium = 0 
+        @plant = 0 
+        @energy = 0 
+        @heat = 0 
     end
 
+    def production
+        @money += @money_production 
+        @steel += @steel_production 
+        @titanium += @titanium_production 
+        @plant += @plant_production 
+        @energy += @energy_production 
+        @heat += @heat_production 
+    end
+
+    def play
+        p "#{player} is playing" 
+    end
 end
--- game.rb
+++ game.rb
@@ -1,7 +1,14 @@
+require_relative 'corporation'
+
 class Game
+    attr_reader :player1, :player2, :current_turn 
     attr_reader :oxygen_level, :temperature_level, :ocean_count 
 
     def initialize
+        @player1 = Corporation.new("Player 1")
+        @player2 = Corporation.new("Player 2")
+        @current_turn = 1 
+
         @oxygen_level = 0 
         @temperature_level = -30 
         @ocean_count = 0
@@ -11,6 +18,22 @@ class Game
         oxygen_level == 14 && temperature_level == 8 && ocean_count == 9 
     end
 
-    def play 
+    def switch_turn
+        current_turn == 1 ? 2 : 1 
+    end
+
+    def play
+        until game_over? 
+            p "Terraforming Mars has begun! Choose a corporation"
+            if current_turn == 1 
+                @player1.play 
+            else  
+                @player2.play 
+            end
+        end 
     end
 end
+
+
+g = Game.new 
+g.play 
